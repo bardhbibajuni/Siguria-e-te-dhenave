3b994cc8ef072a45c033be32d7af50b0dda1c6b7
ahmhxj <ahmethoxhaj630@gmail.com>
2026-04-19 21:24:55 +0200

    decrypted
    decrypted

--- decrypt.py
+++ decrypt.py
@@ -1,1 +1,20 @@
-#test
+def decrypt(encrypted_message, lines):
+    result = []
+
+    for item in encrypted_message:
+        if item == "NOT FOUND":
+            result.append("NOT FOUND")
+        else:
+            line_num, word_index = item
+
+            if 0 <= line_num < len(lines):
+                words = lines[line_num].split()
+
+                if 0 <= word_index < len(words):
+                    result.append(words[word_index])
+                else:
+                    result.append("NOT FOUND")
+            else:
+                result.append("NOT FOUND")
+
+    return " ".join(result)
